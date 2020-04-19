c1de0379b6ebc8b043266251122c34fbc8467346
pavel88dppk <pavel88dppk@users.noreply.github.com>
2020-04-19 08:22:16 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -76,7 +76,7 @@ int main(int argc, string argv[])
             if (!vote(i, j, name))
             {
                 printf("Invalid vote.\n");
-                return 4;
+                return false;
             }
         }
 
