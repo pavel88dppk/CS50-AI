9470ace9412ec6f4ffe659c1503317a72c682e6c
pavel88dppk <pavel88dppk@users.noreply.github.com>
2020-04-19 08:11:34 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     // TODO
    for (int i = 0; i < voter_count; i++)
    {
-       if (strcmp(name, candidates[i].name) == 1)
+       if (strcmp(name, candidates[i].name) == 0)
        {
            preferences[voter][rank] = i;
            return true;
