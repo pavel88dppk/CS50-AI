8d846db990586c8cc8afe2cf90bd4e8762ec7632
pavel88dppk <pavel88dppk@users.noreply.github.com>
2020-05-02 12:43:48 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<stdint.h>
+
 typedef uint8_t byte;
 int main(int argc, char *argv[])
 {
@@ -8,24 +10,27 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover infile\n");
         return 1;
     }
-    char *infile = argv[1]
+    char *infile = argv[1];
     FILE *inpointer = fopen(argv[0], "r");
     if (inpointer == NULL)
     {
-        printf("Could not open")
+        printf("Could not open");
         return 1;
     }
-    byte buffer[512]
+    byte buffer[512];
     int i = 0;
-    for (int n = 0; n < memory; n++)
-    {
-        if (buffer[n][1] == 0xff && buffer[n][1] = 0xd8 && buffer[n][2] = 0xff && buffer[n][2] = 0xe0)
+    char* filename = NULL;
+    FILE *outpointer = NULL;
+    while (&free)
+    {       
+        fread(buffer, sizeof (byte), 512, inpointer);
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             i++;
-            sprintf( filename ,"03i.jpg", i)
-            fread(buffer, sizeof byte, 512, inpointer);
-            
+            sprintf( filename, "%03i.jpg", i);
+            outpointer = fopen (filename, "w");
         }
+        
     }
     
 }
