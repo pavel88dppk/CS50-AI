a6d73a3d95c3bda08d1e1b0a93785d7e6b4dd743
pavel88dppk <pavel88dppk@users.noreply.github.com>
2020-05-05 20:24:46 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 {
      if (argc != 2)
     {
-        fprintf(stderr, "Usage: ./recover infile\n");
+        fprintf(stderr, "Usage: recover infile\n");
         return 1;
     }
     
